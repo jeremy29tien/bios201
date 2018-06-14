78e039417216d326131b7b23ce8b896a0bc91d3b
jeremy29tien <jeremy29tien@gmail.com>
2018-06-13 22:47:07 -0700

    updated to end of section 1

--- workshop2/workshop2.sh
+++ workshop2/workshop2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#GenePred format
 less -S annotation/UCSC_table_browser_chr10.txt
 grep "SFTPA2" annotation/UCSC_table_browser_chr10.txt | column -t | less -S
 
+#GTF/GFF format
+less -S annotation/gencode.v19.annotation_chr10.gtf
+grep "SFTPA2" annotation/gencode.v19.annotation_chr10.gtf | less -S
 
+#Spliced Alignment and read counting
