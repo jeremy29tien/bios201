23f880ffe1454d343366f1a2919f474da1d7ec77
jeremy29tien <jeremy29tien@gmail.com>
2018-06-19 13:41:46 -0700

    worked on fragment size distribution

--- workshop4/workshop4.sh
+++ workshop4/workshop4.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+#Look at fragment size
+# EXAMPLE FORMAT - DO NOT RUN
+# bamPEFragmentSize -b <bamfiles> \
+#                  -hist <plot output filename> \
+#                  --maxFragmentLength <maximum fragment length> \
+#                  -T <plot title>
+
+##Don't use below, the Donor7256-NK.chr4.bam file is corrupted
+#bamPEFragmentSize -b *.bam -hist fragmentSizes.png --maxFragmentLength 500 \
+#                  -T "Fragment sizes of ATAC-seq data"
+
+bamPEFragmentSize -b Donor2596-NK.chr4.bam Donor4983-HSC.chr4.bam Donor5483-NK.chr4.ba -hist fragmentSizes.png --maxFragmentLength 500 -T "Fragment sizes of ATAC-seq data"
+
+
